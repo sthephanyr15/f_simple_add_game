01a1b536a7143df5b9115829da514bcf81df0ff4
sthephanyr15 <sthephanysabogal@gmail.com>
2022-09-12 22:25:43 -0500

    Some testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -25,5 +25,28 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    //verify that the refresh button works
+    await tester.tap(find.byIcon(Icons.refresh));
+    expect(find.text('0'), findsOneWidget);
   });
+
+  // testWidgets('Sum widget works', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //   const op1 = 1;
+  //   const op2 = 5;
+  //   const rta = op1 + op2;
+  //   //verify that the sum is correct
+  //   expect(find.text(op1.toString()), findsOneWidget);
+  //   expect(find.text(op2.toString()), findsOneWidget);
+
+  //   List<int> vectorTest = [];
+  //   vectorTest.clear();
+  //   vectorTest.add(rta);
+  //   vectorTest.add(rta + 1);
+  //   vectorTest.add(rta - 1);
+  //   vectorTest.shuffle();
+
+  //   await tester.tap(find.byWidget(vectorTest));
+  // });
 }
